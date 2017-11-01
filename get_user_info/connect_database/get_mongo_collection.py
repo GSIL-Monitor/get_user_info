5eebbf8fd1358634829956877fd64d40bd78b124
andpay <andpay@andpaydeMacBook-Air-9.local>
2017-11-01 17:26:10 +0800

    modify mongopath

--- get_user_info/connect_database/get_mongo_collection.py
+++ get_user_info/connect_database/get_mongo_collection.py
@@ -10,8 +10,8 @@ from get_user_info.config import init_app
 def get_lrds_maindoc():
     init_app()
 
-    mongodb_path_lrds = bootstrap.ti_config_service.get_value('get_user_info.mongodb_path_lrds', default='/Users/andpay/PycharmProjects/get_user_info/mongo-lrds')
-    mongodb_name_lrds = bootstrap.ti_config_service.get_value('get_user_info.mongodb_name_lrds', default='mongo-lrds')
+    mongodb_path_lrds = bootstrap.ti_config_service.get_value('get_user_info.mongodb_path_lrds', default='/Users/andpay/PycharmProjects/get_user_info/ac_lrds_db')
+    mongodb_name_lrds = bootstrap.ti_config_service.get_value('get_user_info.mongodb_name_lrds', default='ac_lrds_db')
     db = MongoTemplate.get_database(mongodb_path_lrds, mongodb_name_lrds)
 
     #print(db.collection_names(include_system_collections=False))  查询数据库中的表
@@ -27,9 +27,9 @@ def get_cif_partyadditioninfo():
     init_app()
 
     mongodb_path_cif = bootstrap.ti_config_service.get_value('get_user_info.mongodb_path_cif',
-                                                         default='/Users/andpay/PycharmProjects/score_card_end/mongo-cif')
+                                                         default='/Users/andpay/PycharmProjects/score_card_end/ac_cif_db')
 
-    mongodb_name_cif = bootstrap.ti_config_service.get_value('get_user_info.mongodb_name_cif', default='mongo-cif')
+    mongodb_name_cif = bootstrap.ti_config_service.get_value('get_user_info.mongodb_name_cif', default='ad_cif_db')
     db = MongoTemplate.get_database(mongodb_path_cif, mongodb_name_cif)
     #print(db.collection_names(include_system_collections=False))  查询数据库中的表
 
@@ -41,9 +41,9 @@ def get_cif_partyadditioninfo():
 def get_psns_phonecontact():
     init_app()
     mongodb_path_psns = bootstrap.ti_config_service.get_value('get_user_info.mongodb_path_psns',
-                                                         default='/Users/andpay/PycharmProjects/score_card_end/mongo-psns')
+                                                         default='/Users/andpay/PycharmProjects/score_card_end/ac_psns_db')
 
-    mongodb_name_psns = bootstrap.ti_config_service.get_value('get_user_info.mongodb_name_psns', default='mongo-psns')
+    mongodb_name_psns = bootstrap.ti_config_service.get_value('get_user_info.mongodb_name_psns', default='ac_psns_db')
     db = MongoTemplate.get_database(mongodb_path_psns, mongodb_name_psns)
     #print(db.collection_names(include_system_collections=False))  查询数据库中的表
 
