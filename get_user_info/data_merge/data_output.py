37d3033bfb1730cf7229a818c2f8b2a508253d07
andpay <andpay@andpaydeMacBook-Air-9.local>
2017-11-07 17:21:16 +0800

    finsh effciency

--- get_user_info/data_merge/data_output.py
+++ get_user_info/data_merge/data_output.py
@@ -17,15 +17,10 @@ def out_put_run():
     init_app()
     logger = logging.getLogger(__name__)
 
-    startime=time.time()
+    startime=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
     logger.info('to get m2_df begin')
 
-    #m2_df=get_cif_M2()
-
-    middle_time_1=time.time()
-
-    logger.info('get mongodb data, fromTime=[%s], toTime=[%s].' % (startime, middle_time_1))
-
+    m2_df=get_cif_M2()
     mongo_lrds=get_lrds_maindoc()
 
 
@@ -33,18 +28,17 @@ def out_put_run():
               'overdue_num','creditcard_userate','inquiry_num','zm_score','contacts',
               'td_score']
 
+    count=0
+    data_soure=[]
     result_list = []
     #for item in mongo_lrds.find({'crtTime':{'$gte':datetime(2017,1,1)}}):
     #for item in collection.find({'loanApplyInfo.data.partyId':{'$in':partyid_list}}):
     #for item in mongo_lrds.find().sort('crtTime',-1).limit(10):
-    logger.info('star loop')
-    count=0
-    data_soure=[]
     for item in mongo_lrds.find(no_cursor_timeout=True).batch_size(500):
         data_soure.append(item)
         count=count+1
-        if count==50:
-            print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()))
+        if count==500:
+            #print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()))
             for item in data_soure:
                 merge_dict=data_merge(item)
                 print(merge_dict)
@@ -58,9 +52,6 @@ def out_put_run():
             data_soure=[]
             count=0
 
-    middle_time_2=time.time()
-    logger.info('get mongodb data, fromTime=[%s], toTime=[%s].' % (middle_time_1, middle_time_2))
-
     all_info_df=pd.DataFrame(result_list,columns=key_list)
 
     #选取最大applyID
@@ -72,9 +63,9 @@ def out_put_run():
 
     #与逾期数据融合
     end_all_info_df=pd.merge(m2_df,end_all_info_df,on='partyid',how='left')
-    endtime=time.time()
 
-    logger.info('data handle, fromTime=[%s], toTime=[%s].' % (middle_time_2, endtime))
+    endtime=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+    logger.info('data handle, fromTime=[%s], toTime=[%s].' % (startime, endtime))
 
     return end_all_info_df
 
@@ -94,5 +85,5 @@ def email_task():
 
     EmailSend.send_email(subject, to_addrs, body_text, attachment_files=[attachment_file])
 
-out_put_run()
+
 
